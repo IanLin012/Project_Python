34e892d0bacbd40ecc36bd084cc577372c3683ae
Ian <dr.linian@gmail.com>
2024-05-30 18:22:12 +0800

    update

--- Youtube_to_mp3.py
+++ Youtube_to_mp3.py
@@ -1,8 +1,8 @@
-import os # 作業系統相關模組(常用於檔案複製、修改、查詢)
-os.chdir('C:\\Users\\drlin\\Music\\林俊傑') # 改變工作目錄到指定的路徑
+import os
+os.chdir('C:\\Users\\drlin\\Music')
 
 from pytube import YouTube
-yt = YouTube('https://www.youtube.com/watch?v=JJWHIWBH8Kk&list=PLsHtPsJH4YhjjEhIP3rrN-MKRyoNb5Giu&index=18&ab_channel=JJLin-Topic', use_oauth=True, allow_oauth_cache=True)
+yt = YouTube('https://www.youtube.com/watch?v=ZsbaD7-4bgc&ab_channel=JJLin%E6%9E%97%E4%BF%8A%E5%82%91', use_oauth=True, allow_oauth_cache=True)
 print('Downloading...')
-yt.streams.filter().get_audio_only().download(filename='願與愁 林俊傑.mp3') # 下載音檔，以filename做為檔名
+yt.streams.filter().get_audio_only().download(filename='裹著心的光.mp3')
 print('Download successful!')
