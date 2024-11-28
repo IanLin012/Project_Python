65da641448bd57b801bde367224a5d47494a201c
Ian <dr.linian@gmail.com>
2024-11-29 00:35:05 +0800

    move files location

--- 程式設計應用實務/Lesson9/Practice1_九九乘法表/九九乘法表.py
+++ 程式設計應用實務/Lesson9/Practice1_九九乘法表/九九乘法表.py
@@ -1,4 +1,4 @@
-file = open(r"Project_Python\程式設計應用實務\Course\Lesson9\Practice1_九九乘法表\output.txt", "w", encoding='utf-8') # 以寫入模式打開檔案
+file = open(r"Project_Python\程式設計應用實務\Lesson9\Practice1_九九乘法表\output.txt", "w", encoding='utf-8') # 以寫入模式打開檔案
 
 for i in range(1, 10):
     for j in range(1, 10):
