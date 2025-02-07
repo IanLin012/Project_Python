ae6f421da335505e7e10fdbec30cd67b8b0c3968
Ian <dr.linian@gmail.com>
2025-02-07 21:33:08 +0800

    upload congestion & normal weighted

--- 2025_Route_Geometry/csv_filter.py
+++ 2025_Route_Geometry/csv_filter.py
@@ -1,13 +1,23 @@
 import pandas as pd
 
-# Load the CSV data into a DataFrame
-df = pd.read_csv('Project_Python\\2025_Route_Geometry\\車速中位數\\20250102.csv')
+data = pd.read_csv('Project_Python\\2025_Route_Geometry\\車速中位數\\202501_normalspeed.csv')
 
-# List of values to filter the second and third columns
-filter_values = ["03F2066S" ,"03F2079S" ,"03F2066N" ,"03F2078N" ,"03F2100S" ,"03F2129S" ,"03F2100N" ,"03F2125N" ,"03F2152S" ,"03F2194S" ,"03F2153N" ,"03F2194N"]
+#filter_values = ["03F2066S" ,"03F2079S" ,"03F2066N" ,"03F2078N" ,"03F2100S" ,"03F2129S" ,"03F2100N" ,"03F2125N" ,"03F2152S" ,"03F2194S" ,"03F2153N" ,"03F2194N"]
+#filtered_df = df[df.iloc[:, 1].isin(filter_values) & df.iloc[:, 2].isin(filter_values)]
 
-# Filter the rows where both the second and third columns contain any of the values in filter_values
-filtered_df = df[df.iloc[:, 1].isin(filter_values) & df.iloc[:, 2].isin(filter_values)]
+#filtered_data = data[data['Speed'] >= 80]
 
-# Save the filtered DataFrame to a new CSV file
-filtered_df.to_csv('Project_Python\\2025_Route_Geometry\\車速中位數\\20250102_new.csv', index=False, header=False)
+weighted_avg_speed = (
+    data
+    .groupby(['GantryFrom', 'GantryTo', 'VehicleType'], as_index=False)
+    .apply(lambda group: pd.DataFrame({
+        'GantryFrom': [group['GantryFrom'].iloc[0]],
+        'GantryTo': [group['GantryTo'].iloc[0]],
+        'VehicleType': [group['VehicleType'].iloc[0]],
+        'Speed': [(group['Speed'] * group['Volume']).sum() / group['Volume'].sum()],
+        'Volume': [group['Volume'].sum()]  # 總量
+    }))
+    .reset_index(drop=True)  # 移除多餘的索引
+)
+
+weighted_avg_speed.to_csv('Project_Python\\2025_Route_Geometry\\車速中位數\\202501_normalspeed_weighted.csv', index=False, header=True)
